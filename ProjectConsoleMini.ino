bf54772dc653239d4b67ce366e6bd655fca23fdf
aavart sharma <rockstarfan0369@gmail.com>
2024-09-03 20:57:55 +0530

    Galaga has been uploaded
    Galaga is done with memory safe code

--- ProjectConsoleMini.ino
+++ ProjectConsoleMini.ino
@@ -17,14 +17,23 @@
 #define MAX_SHOOT 10
 #define MAX_ENEMY 6
 
+
 // Create the U8g2 object for SH1106 128x64 I2C display
+// U8G2_SH1106_128X64_NONAME_2_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-String state = "";
-const char MainMenu[] = "Main Menu";
-const char Galaga[] = "Galaga";
-const char Snake[] = "Snake";
-const char Flappy_Brid[] = "Flappy Brid";
+int state;
+const char Made[] PROGMEM = "Made By"; 
+const char NAME[] PROGMEM =  "Vart Innovations";
+const char PressB[] PROGMEM = "Press Start Button!";
+// const char MainMenu[] PROGMEM = "Main Menu";
+// const char Galaga[] = "Galaga";
+// const char Snake[] = "Snake";
+// const char Flappy_Brid[] = "Flappy Brid";
+const char MainMenu = 1;
+const int Galaga = 2;
+const int Snake = 3;
+const int Flappy_Brid = 4;
 
 struct DirectVector
 {
@@ -36,6 +45,7 @@ int arraySize =0;
 
 DirectVector enemyList[MAX_ENEMY];
 int arraySizeEnemy =0;
+char buffer[20];
 
 void setup() 
 {
@@ -50,15 +60,16 @@ void setup()
 
   pinMode(BUZZER, OUTPUT);
 
-  
 
   Serial.begin(9600);
   u8g2.begin();
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_helvB08_tf);
-  u8g2.drawStr(5,20,"Made By");
+  strcpy_P(buffer, Made);
+  u8g2.drawStr(5,20,buffer);
   u8g2.setFont(u8g2_font_helvB10_tf);
-  u8g2.drawStr(5, 45, "Vart Innovations");
+  strcpy_P(buffer, NAME);
+  u8g2.drawStr(5, 45,buffer);
   u8g2.sendBuffer();
   delay(2000);
   u8g2.clearBuffer();
@@ -66,22 +77,24 @@ void setup()
   delay(1000);
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_helvB08_tf);
-  u8g2.drawStr(10,40,"Press Start Button!");
+  strcpy_P(buffer, PressB);
+  u8g2.drawStr(10,40,buffer);
   u8g2.sendBuffer();
   while (true)
   {
     delay(100);
     if(digitalRead(MENU_START))
     {
+      // strcpy_P(buffer, MainMenu);
       state = MainMenu;
-      Serial.print(MainMenu);
       break;
     }
   }
 }
 void loop() 
 {
-  Serial.println(F("looppp"));
+  // strcpy_P(buffer, MainMenu);
+  Serial.println(state == MainMenu);
   if(state == MainMenu)
   {
     int yc = 1;
@@ -89,11 +102,14 @@ void loop()
     {
       u8g2.clearBuffer();
       u8g2.setFont(u8g2_font_helvB10_tf);
-      u8g2.drawStr(15,15,Galaga);
+      strcpy_P(buffer, (PGM_P)F("Galaga"));
+      u8g2.drawStr(15,15,buffer);
       u8g2.drawLine(00, 20, 127, 20);
-      u8g2.drawStr(15,40,(Snake));
+      strcpy_P(buffer, (PGM_P)F("Snake"));
+      u8g2.drawStr(15,40,buffer);
       u8g2.drawLine(00, 45, 127, 45);
-      u8g2.drawStr(15,60,(Flappy_Brid));
+      strcpy_P(buffer, (PGM_P)F("Flappy Brid"));
+      u8g2.drawStr(15,60,buffer);
       u8g2.drawLine(00, 63, 127, 63);
       u8g2.drawDisc(111,11*(yc*2-1), 5);
       u8g2.sendBuffer();
@@ -126,6 +142,12 @@ void loop()
     }
     if(state == Galaga)
     {
+      // for (int i = 0; i < MAX_SHOOT; i++) 
+      // {
+      //   shotList[i] = 0;
+      // }
+      arraySize = 0;
+      arraySizeEnemy = 0;
       for(int i = 1; i < 4; i++)
       {
         u8g2.clearBuffer();
@@ -146,6 +168,10 @@ void loop()
       playerPosition.y = 32;
       while(true)
       {
+        if(state == MainMenu)
+        {
+          break;
+        }
         u8g2.clearBuffer();
         
         u8g2.drawLine(0, 0, 127,0);
@@ -177,17 +203,16 @@ void loop()
           newShot.y = playerPosition.y;
           addElement(newShot);
         }
-        if(random(5) ==0 && millis() % 100 <= 10)
+        if(random(5) == 0 && millis() % 500 <= 20)
         {
           DirectVector place;
           place.x = 115;
-          place.y = random(8,24);
+          place.y = random(8,56);
           addEnemy(place);
         }
         Draw_player(playerPosition);
         for (int i = 0; i < arraySize; i++) 
         {
-          //Serial.println(myArray[i].data);
           DrawShot(shotList[i]);
           shotList[i].x += 2;
           if(shotList[i].x >= 128)
@@ -199,15 +224,32 @@ void loop()
         for (int i = 0; i < arraySizeEnemy; i++)
         {
           DrawEnemy(enemyList[i]);
-          enemyList[i].x -= 2;
+          // Serial.print(enemyList[i].x);
+          // Serial.print("     ");
+          // Serial.println(enemyList[i].y);
+          enemyList[i].x -= 1;
           if(enemyList[i].x <= 20)
           {
             // game over
-            Serial.print(F("game over"));
+            u8g2.clearBuffer();
+            u8g2.setFont(u8g2_font_helvB14_tf);
+            strcpy_P(buffer, (PGM_P)F("Game Over!"));
+            u8g2.drawStr(10,32,buffer);
+            u8g2.sendBuffer();
+            delay(5000);
+            state = MainMenu;
+            break;
           }
           for (int j =0; j < arraySize; j++)
           {
-            if((enemyList[i].x - 5) > shotList[j].x > (enemyList[i].x - 5) && (enemyList[i].y - 5) > shotList[j].y > (enemyList[i].y - 5))
+            // Serial.print(enemyList[i].y + 5 );
+            // Serial.print("   ");
+            // Serial.print(shotList[j].y );
+            // Serial.print("   ");
+            // Serial.println(enemyList[i].y - 5);
+            // Serial.print("   ");
+            // Serial.println(((enemyList[i].y + 5) > shotList[j].y > (enemyList[i].y - 5)));
+            if(((enemyList[i].x + 5) > shotList[j].x) && (shotList[j].x > (enemyList[i].x - 5)) && ((enemyList[i].y + 5) > shotList[j].y) && (shotList[j].y > (enemyList[i].y - 5)))
             {
               removeElement(j);
               removeEnemy(i);
@@ -255,9 +297,9 @@ void addElement(DirectVector newElement)
 
 void addEnemy(DirectVector newElement) 
 {
-  if (arraySizeEnemy < MAX_SHOOT) 
+  if (arraySizeEnemy < MAX_ENEMY) 
   {
-    shotList[arraySizeEnemy] = newElement;
+    enemyList[arraySizeEnemy] = newElement;
     arraySizeEnemy++;
   }
 }
@@ -300,7 +342,7 @@ void DrawShot(DirectVector pos)
 
 void DrawEnemy(DirectVector pos)
 {
-  u8g2.drawTriangle(pos.x+5, pos.y, pos.x+5, pos.y +5, pos.x -5, pos.y-5);
+  u8g2.drawTriangle(pos.x-5, pos.y, pos.x+5, pos.y +5, pos.x +5, pos.y-5);
 }
 
 DirectVector Nav_Button()
